1e59d7b989cbda59825fe9d4d0600f0a36409c3d
DresmorAlakazard <DresmorAlakazard@users.noreply.github.com>
2018-06-28 12:07:14 +0200

    main() has no scope variable anymore (bug fix)

--- lolz.cpp
+++ lolz.cpp
@@ -10,6 +10,22 @@
 
 using namespace std;
 
+enum {
+    STATUS_FIND,
+    STATUS_SCAN,
+    STATUS_LISTEN,
+    STATUS_READY
+};
+
+static MSG msg;
+static vector<BYTE> chunk;
+static vector<SIZE_T> test_list;
+static BOOL textUpdating = FALSE;
+static HANDLE cmdthr;
+static HWND hooker;
+static SYSTEM_INFO sysinfo;
+static WNDCLASS hookerClass;
+static RAWINPUTDEVICE RawInputDevice;
 static BOOL retry = FALSE;
 static HWND window;
 static DWORD pid;
@@ -22,13 +38,7 @@ static INT delta = 0;
 static BOOL beep;
 static FLOAT zoom = 0;
 static FLOAT zoomMin = 1000.0f;
-static FLOAT zoomMax = 2250.0f + WHEEL_DELTA * 80;
-enum {
-    STATUS_FIND,
-    STATUS_SCAN,
-    STATUS_LISTEN,
-    STATUS_READY
-};
+static FLOAT zoomMax = 2250.0f + WHEEL_DELTA * 40;
 static INT status = STATUS_FIND;
 static CONST CHAR *statusString[] = {
     [STATUS_FIND] = "Finding game window...",
@@ -37,9 +47,6 @@ static CONST CHAR *statusString[] = {
     [STATUS_READY] = "Ready!"
 };
 
-// Should I use mutex? ...
-static BOOL textUpdating = FALSE;
-
 BOOL IsProcessActive();
 LRESULT CALLBACK HookProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 BOOL IsZoomValue(FLOAT value);
@@ -47,26 +54,22 @@ DWORD WINAPI CommandsThread(LPVOID);
 VOID UpdateText();
 
 int main() {
-    static SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
-
-    static WNDCLASS hookerClass;
     memset(&hookerClass, 0, sizeof(hookerClass));
     hookerClass.hInstance = GetModuleHandle(NULL);
     hookerClass.lpszClassName = "lolz-hooker";
     hookerClass.lpfnWndProc = HookProc;
     if (!RegisterClass(&hookerClass)) printf("%d\n", GetLastError());
 
-    static HWND hooker = CreateWindow(hookerClass.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, 0, hookerClass.hInstance, 0);
+    hooker = CreateWindow(hookerClass.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, 0, hookerClass.hInstance, 0);
 
-    static RAWINPUTDEVICE RawInputDevice;
     RawInputDevice.usUsagePage = 1;
     RawInputDevice.usUsage = 2;
     RawInputDevice.dwFlags = RIDEV_NOLEGACY | RIDEV_INPUTSINK;
     RawInputDevice.hwndTarget = hooker; 
     if (!RegisterRawInputDevices(&RawInputDevice, 1, sizeof(RAWINPUTHEADER))) printf("%d\n", GetLastError());
 
-    static HANDLE cmdthr = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) CommandsThread, NULL, 0, NULL);
+    cmdthr = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) CommandsThread, NULL, 0, NULL);
 
     while (run) {
         list.clear();
@@ -86,25 +89,23 @@ int main() {
 
         if (!IsProcessActive()) continue;
 
-        {
-            vector<BYTE> chunk;
-            for (SIZE_T address = (SIZE_T) sysinfo.lpMinimumApplicationAddress; address < (SIZE_T) sysinfo.lpMaximumApplicationAddress;) {
-                MEMORY_BASIC_INFORMATION info;
-                if (VirtualQueryEx(process, (LPCVOID) address, &info, sizeof(info)) != sizeof(info) || info.RegionSize > 0x7fffffff) break;
-                if (info.Protect == PAGE_READWRITE && info.State == MEM_COMMIT) {
-                    if (chunk.capacity() < info.RegionSize) chunk.resize(info.RegionSize);
-                    ReadProcessMemory(process, info.BaseAddress, (LPVOID) chunk.data(), info.RegionSize, NULL);
-                    SIZE_T terminal = info.RegionSize - sizeof(heading);
-                    #pragma omp parallel for shared(list)
-                    for (SIZE_T i = 0; i <= terminal; ++i) {
-                        memcpy(heading, chunk.data() + i, sizeof(heading));
-                        if (heading[0] == 2250.0f && heading[1] == 2250.0f) {
-                            list.push_back(address + i);
-                        }
+        for (SIZE_T address = (SIZE_T) sysinfo.lpMinimumApplicationAddress; address < (SIZE_T) sysinfo.lpMaximumApplicationAddress;) {
+        // for (SIZE_T address = (SIZE_T) 0; address < (SIZE_T) 0x7fffffff;) {
+            MEMORY_BASIC_INFORMATION info;
+            if (VirtualQueryEx(process, (LPCVOID) address, &info, sizeof(info)) != sizeof(info) || info.RegionSize > 0x7fffffff) break;
+            if (info.Protect == PAGE_READWRITE && info.State == MEM_COMMIT) {
+                if (chunk.capacity() < info.RegionSize) chunk.resize(info.RegionSize);
+                ReadProcessMemory(process, info.BaseAddress, (LPVOID) chunk.data(), info.RegionSize, NULL);
+                SIZE_T terminal = info.RegionSize - sizeof(heading);
+                #pragma omp parallel for shared(list)
+                for (SIZE_T i = 0; i <= terminal; ++i) {
+                    memcpy(heading, chunk.data() + i, sizeof(heading));
+                    if (heading[0] == 2250.0f && heading[1] == 2250.0f) {
+                        list.push_back(address + i);
                     }
                 }
-                address += info.RegionSize;
             }
+            address += info.RegionSize;
         }
 
         if (list.empty()) { Sleep(1); continue; }
@@ -114,24 +115,22 @@ int main() {
 
         if (beep) MessageBeep(MB_OK);
 
-        MSG msg;
-
         while (run && IsProcessActive() && list.size() > 1) {
             if (retry) break;
             while (PeekMessage(&msg, hooker, 0, 0, PM_REMOVE)) DispatchMessage(&msg);
             if (run && IsProcessActive()) {
                 if (triggered) {
                     triggered = FALSE;
-                    vector<SIZE_T> new_list;
+                    test_list.clear();
                     for (SIZE_T address: list) {
                         ReadProcessMemory(process, (LPCVOID) address, heading, sizeof(heading), NULL);
                         if (IsZoomValue(heading[0]) && IsZoomValue(heading[1])) {
                         // if (heading[0] != 2250.0f && heading[1] != 2250.0f) {
-                            new_list.push_back(address);
+                            test_list.push_back(address);
                         }
                     }
-                    if (new_list.size()) {
-                        list = new_list;
+                    if (test_list.size()) {
+                        list = test_list;
                     }
                 }
                 Sleep(1);
